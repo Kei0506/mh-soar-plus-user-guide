56c1cfac1dabe532b74f642b0628f1020e3546db
Kei0506 <Kei0506@users.noreply.github.com>
2026-01-20 03:42:06 +0000

    deploy: 4a1257dd64c98587e32edf99a471e00370d23ebd

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c38675f2.js',
+                path_to_root + 'searchindex-4beb28b8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
